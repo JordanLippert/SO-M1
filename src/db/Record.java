dfb5c5c6221d089ceae235ccfefd3a2af3c575cb
JordanLippert <lippertx11@gmail.com>
2025-04-12 22:47:19 -0300

    feat: criando e implementando classe Record para o banco de dados

--- src/db/Record.java
+++ src/db/Record.java
@@ -1,4 +1,28 @@
 package db;
 
 public class Record {
+    private int id;
+    private String nome;
+
+    public Record(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString(){
+        return "ID=" + id + ", Nome='" + nome + "'";
+    }
 }
