28e73399582d249f30a208a1b4b761e874b3bb66
JordanLippert <lippertx11@gmail.com>
2025-04-14 14:15:18 -0300

    feat: criando e implementando classe ThreadWorker para manipular a base de dados

--- src/server/ThreadWorker.java
+++ src/server/ThreadWorker.java
@@ -1,4 +1,38 @@
 package server;
 
-public class ThreadWorker {
+import db.Database;
+import db.Record;
+
+public class ThreadWorker implements Runnable{
+    private final String command;
+    private final Database db;
+
+    public ThreadWorker(String command, Database db) {
+        this.command = command;
+        this.db = db;
+    }
+
+    @Override
+    public void run() {
+        if (command.startsWith("INSERT")) {
+            String[] parts = command.split(" ",3);
+            int id = Integer.parseInt(parts[1]);
+            String nome = parts[2];
+            db.insert(new Record(id, nome));
+        } else if (command.startsWith("DELETE")) {
+            int id = Integer.parseInt(command.split(" ")[1]);
+            db.delete(id);
+        } else if (command.startsWith("SELECT")) {
+            int id = Integer.parseInt(command.split(" ")[1]);
+            Record record = db.select(id);
+            System.out.println("SELECT: " + (record != null ? record: "ID=" + id + " não encontrado"));
+        } else if (command.startsWith("UPDATE")) {
+            String[] parts = command.split("", 3);
+            int id = Integer.parseInt(parts[1]);
+            String nome = parts[2];
+            db.update(id, nome);
+        } else {
+            System.out.println("Comando inválido: " + command);
+        }
+    }
 }
